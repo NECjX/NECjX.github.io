961ef955dde84207a89b4dc207b51dccfa04b5b1
Kadae <Kadae@users.noreply.github.com>
2024-08-14 16:15:51 +0500

    Updates

--- _nuxt/Bq4qPv-4.js
+++
@@ -1,1 +0,0 @@
-const e=()=>({legacy:!1,locale:"en",messages:{en:{welcomeTitle:"Greatest knowledge"},ru:{welcomeTitle:"Величайшее знание"}}});export{e as default};
---
+++ _nuxt/DDmBYwYp.js
@@ -0,0 +1,1 @@
+import{g as e,c,o as n}from"./COOOzc6K.js";const o={};function r(t,s){return n(),c("div")}const _=e(o,[["render",r]]);export{_ as default};
